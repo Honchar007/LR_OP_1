586452ca155c493b36daf6ffafed84093afd8b24
Andrey <andrey2002335@gmail.com>
2020-02-20 17:47:18 +0200

    Figure out how many teams and fill table

--- LR_OP_1/Program.cs
+++ LR_OP_1/Program.cs
@@ -1,17 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace LR_OP_1
 {
     class Program
     {
+        static string[,] ReadText()
+        {
+            string str = File.ReadAllText("premier_league1.csv");
+            int c=0;
+            // Spliting
+            string[] wordSplit = str.Split(new char[] { ',', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries); 
+            string temp;
+            // figure out how many teams
+            for (int i = 0; i < wordSplit.Length; i++) 
+            {
+                temp = wordSplit[i];
+                if (Char.IsLetter(temp[0]))
+                {
+                    c++;
+                }
+            }
+            int n = int.Parse(wordSplit[0]) + 1;
+          string[,] tabl = new string[c , n];
+            int counter = 1;
+            // fill table
+            for (int i = 0; i < c; i++) 
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    tabl[i,j] = wordSplit[counter];
+                    counter++;
+                }  
+            }
+            return tabl;
+        }
+
         static void Main(string[] args)
         {
-           
+            string[,] num = ReadText();
             Console.WriteLine("HelloWorld");
+            Console.ReadLine();
         }
     }
 }
