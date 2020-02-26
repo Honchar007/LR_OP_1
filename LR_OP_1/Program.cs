b3530ea5d645d339f8062706c07361814cfb478d
Dariia Kariavka <koalaansthen@gmail.com>
2020-02-26 07:16:38 +0200

    Create points matrix, do a check

--- LR_OP_1/Program.cs
+++ LR_OP_1/Program.cs
@@ -6,15 +6,23 @@ namespace LR_OP_1
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            ReadText();
+
+            //DivideOnTwoTables(tabl, tabl.GetLowerBound(0), tabl.Length / tabl.GetLowerBound(0));
+            Console.WriteLine("HelloWorld");
+            Console.ReadLine();
+        }
         static void ReadText()
         {
             string str = File.ReadAllText("premier_league1.csv");
-            int c=0;
-            // Spliting
-            string[] wordSplit = str.Split(new char[] { ',', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries); 
+            int c = 0;
+            // Splitting
+            string[] wordSplit = str.Split(new char[] { ',', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
             string temp;
-            // figure out how many teams
-            for (int i = 0; i < wordSplit.Length; i++) 
+            // figure out how many teams we have
+            for (int i = 0; i < wordSplit.Length; i++)
             {
                 temp = wordSplit[i];
                 if (Char.IsLetter(temp[0]))
@@ -23,20 +31,20 @@ namespace LR_OP_1
                 }
             }
             int n = int.Parse(wordSplit[0]) + 1;
-          string[,] tabl = new string[c , n];
+            string[,] tabl = new string[c, n];
             int counter = 1;
             // fill table
-            for (int i = 0; i < c; i++) 
+            for (int i = 0; i < c; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    tabl[i,j] = wordSplit[counter];
+                    tabl[i, j] = wordSplit[counter];
                     counter++;
-                }  
+                }
             }
             DivideOnTwoTables(tabl, c, n);
         }
-        static void DivideOnTwoTables(string[,] tabl,int teamsN,int matchN)
+        static void DivideOnTwoTables(string[,] tabl, int teamsN, int matchN)
         {
             int[,] ResultOfCurrentTeams = new int[teamsN, matchN];
             int[,] ResultOfOpponentTeams = new int[teamsN, matchN];
@@ -48,21 +56,30 @@ namespace LR_OP_1
                     temp = tabl[i, j];
                     if (Char.IsNumber(temp[0]))
                     {
-                        ResultOfCurrentTeams[i, j - 1] = temp[0] - '0';
-                        ResultOfOpponentTeams[i, j - 1] = temp[2] - '0'; Console.Write(ResultOfOpponentTeams[i, j - 1] + " ");
+                        ResultOfCurrentTeams[i, j-1] = temp[0] - '0'; Console.Write(ResultOfCurrentTeams[i, j-1] + "-");
+                        ResultOfOpponentTeams[i, j-1] = temp[2] - '0'; Console.Write(ResultOfOpponentTeams[i, j-1] + " ");
                     }
                 }
                 Console.WriteLine();
             }
-        }
 
-        static void Main(string[] args)
-        {
-            ReadText();
+            int[] PointList = new int[teamsN];
+            for (int i = 0; i < teamsN; i++)
+            {
+                for (int j = 1; j < matchN; j++)
+                {
+                    if (ResultOfCurrentTeams[i, j-1] > ResultOfOpponentTeams[i, j-1])
+                        PointList[i] += 3;     
+                    if (ResultOfCurrentTeams[i, j-1] == ResultOfOpponentTeams[i, j-1])
+                        PointList[i] += 1;
+                   
+                }
+                Console.WriteLine(PointList[i]);
+                Console.WriteLine();
+            }
+
+
            
-            //DivideOnTwoTables(tabl, tabl.GetLowerBound(0), tabl.Length / tabl.GetLowerBound(0));
-            Console.WriteLine("HelloWorld");
-            Console.ReadLine();
         }
     }
 }
